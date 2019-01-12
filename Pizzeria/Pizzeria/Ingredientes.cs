b81d551ea26bbc577c0005f7964467104d78790a
Yeduin Mateo <el_yeduin_02@hotmail.com>
2019-01-12 18:57:24 -0400

    clase ingrediente con su constructor y getter and setters

---
+++ Pizzeria/Pizzeria/Ingredientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzeria
+{
+    class Ingredientes
+    {
+        private String nombre { get; set; }
+        private int precio { get; set; }
+
+        public Ingredientes(String nombre, int precio)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+        }
+    }
+}
