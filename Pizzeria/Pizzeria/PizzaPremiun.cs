b1a64faff3f9aec64ba1fd430b0bb9baa9930672
Yeduin Mateo <el_yeduin_02@hotmail.com>
2019-01-12 21:48:16 -0400

    Nuevas Clases: PizzaPremium y PizzaSaludable agregadas por medio de herencia.

---
+++ Pizzeria/Pizzeria/PizzaPremiun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzeria
+{
+    class PizzaPremiun : Pizza
+    {
+        public PizzaPremiun(Ingredientes masa, Ingredientes salsa, int precio) : base(salsa, masa, precio)
+        {
+
+            adicionales = new Ingredientes[2];
+            Ingredientes palitosqueso = new Ingredientes("Palitos Rellenos con Queso", 50);
+            Ingredientes bebida = new Ingredientes("Coca-Cola", 30);
+            agregarAdicionales(palitosqueso);
+            agregarAdicionales(bebida);
+        }
+    }
+}
---
+++ Pizzeria/Pizzeria/PizzaSaludable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzeria
+{
+    class PizzaSaludable : Pizza 
+    {
+        public PizzaSaludable(Ingredientes masa, Ingredientes salsa, int precio) : base(salsa, masa, precio)
+        {
+            
+            Ingredientes masaFina = new Ingredientes("Masa Fina", 0);
+            SetMasa(masaFina);
+            adicionales = new Ingredientes[6];
+        }
+    }
+}
